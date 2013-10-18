ec4a73a104e44787f0c6d8dd7550e824035c84ee
Dougal Sutherland <dougal@gmail.com>
2013-10-17 21:19:16 -0400

    little fixes to flann recipe

--- flann/build.sh
+++ flann/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 mkdir build
 cd build
+
+# TODO: add logic here...
 export CC=gcc-4.8
 export LD=gcc-4.8
 export CXX=g++-4.8
+
 cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
          -DBUILD_MATLAB_BINDINGS:BOOL=OFF \
          -DBUILD_PYTHON_BINDINGS:BOOL=ON \
          -DBUILD_EXAMPLES:BOOL=OFF
-# TODO: make python bindings work
 make -j$(python -c 'import multiprocessing as m; print m.cpu_count()') install
