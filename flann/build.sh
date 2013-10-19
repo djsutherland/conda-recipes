c82f20805bc29a3fc190f00ff0a8e4d543e5066b
Dougal Sutherland <dougal@gmail.com>
2013-10-19 19:53:26 -0400

    make flann actually work on osx

--- flann/build.sh
+++ flann/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 mkdir build
 cd build
 
@@ -13,4 +14,34 @@ cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
          -DBUILD_MATLAB_BINDINGS:BOOL=OFF \
          -DBUILD_PYTHON_BINDINGS:BOOL=ON \
          -DBUILD_EXAMPLES:BOOL=OFF
+
 make -j$(python -c 'import multiprocessing as m; print m.cpu_count()') install
+
+if [[ `uname` == Darwin ]]; then
+    # copy in the gcc-4.8 version of openmp, etc
+    # TODO: solve this a better way.
+    cd $PREFIX/lib
+    mkdir flann-support
+
+    libgomp=$(otool -L libflann.dylib | grep -Eo '/.*libgomp.*?\s')
+    libstd=$(otool -L libflann.dylib | grep -Eo '/.*libstdc\+\+.*?\s')
+    libgcc=$(otool -L libflann.dylib | grep -Eo '/.*libgcc_s.*?\s' \
+             | grep -v '^\s*/usr/lib/libgcc_s')
+
+    lgomp=flann-support_$(basename $libgomp)
+    lstd=flann-support_$(basename $libstd)
+    lgcc=flann-support_$(basename $libgcc)
+
+    cp $libgomp $lgomp
+    cp $libstd $lstd
+    cp $libgcc $lgcc
+
+    for x in libflann.dylib libflann_cpp.dylib $lgomp $lstd $lgcc; do
+        install_name_tool -id $PREFIX/lib/$x $x
+        install_name_tool -change $libgomp $PREFIX/lib/$lgomp $x
+        install_name_tool -change $libstd  $PREFIX/lib/$lstd $x
+        install_name_tool -change $libgcc  $PREFIX/lib/$lgcc $x
+    done
+
+    cd -
+fi
