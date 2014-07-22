d6f3a9ebdcdc8b8fab791bcdb0d9a6fb8dd55eba
Dougal J. Sutherland <dougal@gmail.com>
2014-07-22 19:59:38 -0400

    make hsfuap version dynamic :)

--- hsfuap/build.sh
+++ hsfuap/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 HSFUAP_DIR=${HSFUAP_DIR:-$HOME/hsfuap}
+
+date -d "$(git --git-dir=$HSFUAP_DIR/.git log -n1 --format=%ci)" +0.1.0dev%Y.%m.%d.%H.%M > __conda_version__.txt
+
 cp -a $HSFUAP_DIR/* .
 $PYTHON setup.py install
