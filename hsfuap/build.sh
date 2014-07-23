8173e05493efd895534f240c2b3aa377276609d9
Dougal J. Sutherland <dougal@gmail.com>
2014-07-22 20:11:59 -0400

    add hsfuap source as a submodule; hack compilation on osx

--- hsfuap/build.sh
+++ hsfuap/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-HSFUAP_DIR=${HSFUAP_DIR:-$HOME/hsfuap}
+if [[ `uname` == Darwin ]]; then
+    export CC=gcc-4.9
+    export LD=gcc-4.9
+    export CXX=g++-4.9
+fi
 
-date -d "$(git --git-dir=$HSFUAP_DIR/.git log -n1 --format=%ci)" +0.1.0dev%Y.%m.%d.%H.%M > __conda_version__.txt
+HSFUAP_DIR=$RECIPE_DIR/src
+
+date="$(git --git-dir=$HSFUAP_DIR/.git log -n1 --format=%ct)"
+$PYTHON -c "import datetime; print('0.1.0dev{:%Y.%m.%d.%H.%M}'.format(datetime.datetime.fromtimestamp($date)))" > __conda_version__.txt
 
 cp -a $HSFUAP_DIR/* .
 $PYTHON setup.py install
