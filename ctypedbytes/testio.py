e5b39a2c8f3a97b5122ddc3b266b0e791144820b
Dougal J. Sutherland <dougal@gmail.com>
2014-11-13 16:05:33 -0500

    add typedbytes, ctypedbytes packages

---
+++ ctypedbytes/testio.py
@@ -0,0 +1,63 @@
+# copied from the ctypedbytes-0.1.8 source
+# because conda isn't great about this testing case
+import os
+import unittest
+import decimal
+import datetime
+import ctypedbytes as typedbytes
+
+
+class TestIO(unittest.TestCase):
+
+    objects = [True, 1234, 3000000000, 12345L, 1.23, "trala", u'trala',
+                'tra\0la', (1,2,3), [1,2,3,4], {1:2,3:4}, set([1,2,3]),
+                decimal.Decimal("123.456"), datetime.datetime.now(),
+                -40334331270250613356122789195623375063L]
+
+    def testio(self):
+        objects = TestIO.objects
+        file = open("test.bin", "wb")
+        output = typedbytes.Output(file)
+        output.writes(objects)
+        file.close()
+        file = open("test.bin", "rb")
+        input = typedbytes.Input(file)
+        for (index, record) in enumerate(input.reads()):
+            self.assertEqual(objects[index], record)
+        file.close()
+        os.remove("test.bin")
+
+    def testpairio(self):
+        objects = TestIO.objects
+        file = open("test.bin", "wb")
+        output = typedbytes.PairedOutput(file)
+        output.writes(enumerate(objects))
+        file.close()
+        file = open("test.bin", "rb")
+        input = typedbytes.PairedInput(file)
+        for index, record in input.reads():
+            self.assertEqual(objects[index], record)
+        file.close()
+        os.remove("test.bin")
+
+    def testpickle(self):
+        file = open("test.bin", "wb")
+        output = typedbytes.Output(file)
+        output.write("123")
+        output.write(MyClass())
+        file.close()
+        file = open("test.bin", "rb")
+        input = typedbytes.Input(file)
+        self.assertEqual("123", input.read())
+        self.assertEqual(234, input.read().attr)
+        file.close()
+        os.remove("test.bin")
+
+
+class MyClass:
+    attr = 234
+
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestIO)
+    unittest.TextTestRunner(verbosity=2).run(suite)
---
+++ ctypedbytes/testspeed.py
@@ -0,0 +1,76 @@
+# copied from the ctypedbytes-0.1.8 source
+# because conda isn't great about this testing case
+import cStringIO
+import unittest
+import time
+import os
+import ctypedbytes as typedbytes
+from itertools import imap
+
+
+class TestSpeed(unittest.TestCase):
+
+    def testints(self):
+        file = open("speedtest.bin", "wb")
+
+        output = typedbytes.Output(file)
+        t = time.time()
+        output.writes(xrange(100000))
+        print "writing ints:", time.time() - t
+
+        file.close()
+        file = open("speedtest.bin", "rb")
+
+        input = typedbytes.Input(file)
+        t = time.time()
+        for record in input:
+            pass
+        print "reading ints:", time.time() -t
+
+        file.close()
+        os.remove("speedtest.bin")
+
+    def teststrings(self):
+        file = open("speedtest.bin", "wb")
+
+        output = typedbytes.Output(file)
+        t = time.time()
+        output.writes(imap(str, xrange(100000)))
+        print "writing strings:", time.time() - t
+
+        file.close()
+        file = open("speedtest.bin", "rb")
+
+        input = typedbytes.Input(file)
+        t = time.time()
+        for record in input:
+            pass
+        print "reading strings:", time.time() -t
+
+        file.close()
+        os.remove("speedtest.bin")
+
+    def testunicodes(self):
+        file = open("speedtest.bin", "wb")
+
+        output = typedbytes.Output(file)
+        t = time.time()
+        output.writes(imap(unicode, xrange(100000)))
+        print "writing unicodes:", time.time() - t
+
+        file.close()
+        file = open("speedtest.bin", "rb")
+
+        input = typedbytes.Input(file)
+        t = time.time()
+        for record in input:
+            pass
+        print "reading unicodes:", time.time() -t
+
+        file.close()
+        os.remove("speedtest.bin")
+
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestSpeed)
+    unittest.TextTestRunner(verbosity=2).run(suite)
