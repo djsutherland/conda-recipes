47699e244cf64e0a9cdfdc24cb928041e813f793
Dougal J. Sutherland <dougal@gmail.com>
2015-08-03 15:19:05 -0400

    update flann, use conda gcc

--- flann/build.sh
+++ flann/build.sh
@@ -3,8 +3,8 @@
 mkdir build
 cd build
 
-# TODO: add logic here...
 if [[ `uname` == Darwin ]]; then
+    # Make sure we're using conda gcc
     export CC=gcc
     export LD=gcc
     export CXX=g++
@@ -16,32 +16,3 @@ cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX \
          -DBUILD_EXAMPLES:BOOL=OFF
 
 make -j$(python -c 'import multiprocessing as m; print m.cpu_count()') install
-
-if [[ `uname` == Darwin ]]; then
-    # copy in the gcc version of openmp, etc
-    # TODO: solve this a better way.
-    cd $PREFIX/lib
-
-    libgomp=$(otool -L libflann.dylib | grep -Eo '/.*libgomp.*?\s')
-    libstd=$(otool -L libflann.dylib | grep -Eo '/.*libstdc\+\+.*?\s')
-    libgcc=$(otool -L libflann.dylib | grep -Eo '/.*libgcc_s.*?\s' \
-             | grep -v '^\s*/usr/lib/libgcc_s')
-
-    lgomp=flann-support_$(basename $libgomp)
-    lstd=flann-support_$(basename $libstd)
-    lgcc=flann-support_$(basename $libgcc)
-
-    cp $libgomp $lgomp
-    cp $libstd $lstd
-    cp $libgcc $lgcc
-    chmod u+w $lgomp $lstd $lgcc
-
-    for x in libflann.dylib libflann_cpp.dylib $lgomp $lstd $lgcc; do
-        install_name_tool -id $PREFIX/lib/$x $x
-        install_name_tool -change $libgomp $PREFIX/lib/$lgomp $x
-        install_name_tool -change $libstd  $PREFIX/lib/$lstd $x
-        install_name_tool -change $libgcc  $PREFIX/lib/$lgcc $x
-    done
-
-    cd -
-fi
