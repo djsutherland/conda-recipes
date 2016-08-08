5bc5b81921934b56ba17a5f0e46c93df837a0fca
Dougal J. Sutherland <dougal@gmail.com>
2016-08-08 13:59:31 -0400

    hsfuap: build with conda gcc

--- hsfuap/build.sh
+++ hsfuap/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [[ `uname` == Darwin ]]; then
-    export CC=gcc-4.9
-    export LD=gcc-4.9
-    export CXX=g++-4.9
-fi
-
 $PYTHON setup.py egg_info
 sed -n -e 's/^Version: \(.*\)$/\1/p' < hsfuap.egg-info/PKG-INFO > __conda_version__.txt
 
